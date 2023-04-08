fdcc08d8a98a3e51656d01ff53e49b381e06ac50
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2023-04-08 19:42:37 -0300

    resolve binary tree level order traversal

--- leetcode/tree/binary-tree-level-order-traversal.js
+++ leetcode/tree/binary-tree-level-order-traversal.js
@@ -63,3 +63,25 @@ console.log(levelOrder(root))
  */
 const root2 = new TreeNode(1, new TreeNode(2,new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6), new TreeNode(7)));
 console.log(levelOrder(root2))
+
+/**
+Para realizar a travessia em ordem de nível de uma árvore binária, podemos usar o algoritmo Breadth First Search (BFS). 
+Começamos adicionando o nó raiz em uma fila, e enquanto a fila não estiver vazia, retiramos o primeiro elemento da fila, 
+adicionamos seu valor à lista do nível atual e adicionamos seus filhos esquerdo e direito (se houver) à fila.
+
+Podemos repetir esse processo até que a fila esteja vazia, o que significa que percorremos todos os nós da árvore binária.
+
+Neste código, primeiro verificamos se o nó raiz existe. Se não existir, retornamos uma lista vazia, pois não há nós para percorrer.
+
+Em seguida, criamos uma lista vazia chamada result para armazenar o resultado da travessia em ordem de nível e um objeto deque 
+chamado queue para armazenar os nós da árvore binária que queremos percorrer.
+
+Então, entramos em um loop enquanto a fila não estiver vazia. Dentro do loop, obtemos o tamanho do nível atual 
+obtendo o comprimento da fila no início do loop. Em seguida, criamos uma lista vazia chamada current_level para armazenar os valores dos nós no nível atual.
+
+Em seguida, percorremos o número de nós no nível atual, que é igual a level_size. Em cada iteração desse loop, 
+obtemos o primeiro nó na fila usando popleft(), adicionamos seu valor à lista current_level e adicionamos seus filhos esquerdo e direito (se houver) à fila.
+
+Depois de percorrer todos os nós no nível atual, adicionamos a lista current_level à lista result. 
+Repetimos esse processo até que a fila esteja vazia, momento em que retornamos a lista result contendo a travessia em ordem de nível da árvore binária.
+ */
